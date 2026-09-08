85df5a44014ca492e368f286a23576815c0ff682
wibem1 <wibem1@googlemail.com>
2026-09-08 11:12:57 +0200

    Remove obsolete workspace repair compatibility stub

--- workspace-repair-v21.js
+++
@@ -1,7 +0,0 @@
-(()=>{
-'use strict';
-// Kompatibilitätsalias für historische index.html-Stände.
-// Die früher hier enthaltene Logik liegt jetzt in den kanonischen Modulen:
-// shared/experiment-adapter.js und shared/storage-adapter.js.
-window.__compositionLabLegacyWorkspaceAlias=true;
-})();
