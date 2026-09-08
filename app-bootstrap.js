f58300819dccb5c7bcf888eaa0d6e2f1f5941861
wibem1 <wibem1@googlemail.com>
2026-09-08 11:30:38 +0200

    Remove obsolete WebApp bootstrap wrapper

--- app-bootstrap.js
+++
@@ -1,9 +0,0 @@
-(()=>{
-'use strict';
-if(window.__compositionLabConsolidatedLoader)return;
-window.__compositionLabConsolidatedLoader=true;
-const s=document.createElement('script');
-s.async=false;
-s.src='/Composer-Lab/shared/consolidated-bootstrap.js?fresh='+Date.now();
-(document.head||document.body).appendChild(s);
-})();
