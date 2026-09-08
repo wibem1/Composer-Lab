6228aa7149b89c4e8fb6007d5b7287bb84c1d0e6
wibem1 <wibem1@googlemail.com>
2026-09-08 11:12:50 +0200

    Remove obsolete diagnostics compatibility stub

--- diagnostics-v2.js
+++
@@ -1,9 +0,0 @@
-(()=>{
-'use strict';
-if(window.__compositionLabDiagnosticsAlias)return;
-window.__compositionLabDiagnosticsAlias=true;
-const s=document.createElement('script');
-s.async=false;
-s.src='/Composer-Lab/diagnostics.js?fresh='+Date.now();
-(document.head||document.body).appendChild(s);
-})();
