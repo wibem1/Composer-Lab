1152b2290b2e441543f8fa5e9034dbe6f012332f
wibem1 <wibem1@googlemail.com>
2026-09-08 11:12:43 +0200

    Remove obsolete comparison compatibility stub

--- comparison-chat-v27.js
+++
@@ -1,9 +0,0 @@
-(()=>{
-'use strict';
-if(window.__compositionLabBootstrapAlias)return;
-window.__compositionLabBootstrapAlias=true;
-const s=document.createElement('script');
-s.async=false;
-s.src='/Composer-Lab/app-bootstrap.js?fresh='+Date.now();
-(document.head||document.body).appendChild(s);
-})();
